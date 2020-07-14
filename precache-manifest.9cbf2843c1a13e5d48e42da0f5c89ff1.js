a6681f094a73e2cdbe391fb8dc84f7fc1ff1707a
yuri_pashkevich <shmulhe@gmail.com>
2020-07-15 00:01:02 +0300

    Updates

---
+++ precache-manifest.9cbf2843c1a13e5d48e42da0f5c89ff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a7bc500d70b2d95109e2e71ed68840",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "20506af47dafcdf87988",
+    "url": "/news/static/css/main.ac1cf8eb.chunk.css"
+  },
+  {
+    "revision": "61a1a0fbbed19de8b0e1",
+    "url": "/news/static/js/2.bb8d96aa.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/news/static/js/2.bb8d96aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20506af47dafcdf87988",
+    "url": "/news/static/js/main.8b52b85d.chunk.js"
+  },
+  {
+    "revision": "5b584f5963c127323fcd",
+    "url": "/news/static/js/runtime-main.3664c29e.js"
+  }
+]);
